26b947a9f27666404ec86f1ef89a0ad66322a20b
irenemp <munozpe.irene@gmail.com>
2019-01-24 20:51:20 +0100

    boton

--- js/scripts.js
+++ js/scripts.js
@@ -96,4 +96,26 @@ $(".nubes1").css("transform", "translate("+e.pageX * 0.01 +"px, "+e.pageX * 0.02
 
 					$(document).ready(function () {
 
-					    
+						/*********************************************** boton hacia arriba **********************************************/
+						$('.ir-arriba').click(function(){
+								$('body, html').animate({
+										scrollTop: '0px'
+								}, 1000);
+						});
+
+						$(window).scroll(function(){
+								if( $(this).scrollTop() > 4800 ){
+										$('.ir-arriba').slideDown(600);
+								} else {
+										$('.ir-arriba').slideUp(600);
+								}
+						});
+
+						/*hacia abajo*/
+						$('.ir-abajo').click(function(){
+								$('body, html').animate({
+										scrollTop: '1000px'
+								}, 1000);
+						});
+
+				});
